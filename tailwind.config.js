43b3ece06324812fc10920a7bbd49a8163a42bf4
ashiqur <ashiqur@codebehindltd.xyz>
2024-05-22 11:17:37 +0600

    Tailwind CSS installed

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primaryColor': '#AE862D', // rgb(174, 134, 45)
+        'gradient1': 'rgba(52, 58, 64, 0.1)',
+        'gradient2': 'rgba(174, 134, 45, .15)',
+        'galleryGradient': 'rgba(174, 134, 45,.8)',
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+        lg: '0px',
+        xl: '0px',
+        '2xl': '0px'
+      }
+    },
+  },
+  plugins: [],
+}
+
