dd99573ffe1243342a2563753cb55ba0df8581b0
ashiqur <ashiqur@codebehindltd.xyz>
2024-05-22 11:45:22 +0600

    Folder structure changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./*.html", "./dist/*.{html,css,js}"],
   theme: {
     extend: {
       colors: {
