bad83c93db55a0b65120d8992296d26202f510c1
ashiqsetu <setu037@gmail.com>
2024-09-12 07:38:59 +0600

    Offer section design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ module.exports = {
     extend: {
       colors: {
         'primaryColor': '#1DD100', // rgb(29, 209, 0)
-        'primaryColorA10': 'rgba(29, 209, 0, .1)', // rgb(29, 209, 0)
-        'primaryColorA40': 'rgba(29, 209, 0, .4)', // rgb(29, 209, 0)
+        'primaryColorA10': 'rgba(29, 209, 0, .1)',
+        'primaryColorA40': 'rgba(29, 209, 0, .4)',
         'secondaryColor': '#030712', // rgb(3, 7, 18)
-        'secondaryColorA60': 'rgba(3, 7, 18, 0.6)', // rgb(3, 7, 18)
+        'secondaryColorA40': 'rgba(3, 7, 18, 0.4)',
+        'secondaryColorA50': 'rgba(3, 7, 18, 0.5)',
+        'secondaryColorA60': 'rgba(3, 7, 18, 0.6)',
+        'secondaryColorA80': 'rgba(3, 7, 18, 0.8)',
       },
     },
     container: {
