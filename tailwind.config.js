817dfadea2c4a761d924c29c631a899de955cbb7
ashiqsetu <setu037@gmail.com>
2024-09-12 17:27:11 +0600

    Ticket area design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,12 @@ module.exports = {
       colors: {
         'primaryColor': '#1DD100', // rgb(29, 209, 0)
         'primaryColorA10': 'rgba(29, 209, 0, .1)',
+        'primaryColorA15': 'rgba(29, 209, 0, .15)',
         'primaryColorA40': 'rgba(29, 209, 0, .4)',
         'secondaryColor': '#030712', // rgb(3, 7, 18)
+        'secondaryColorA05': 'rgba(3, 7, 18, 0.05)',
+        'secondaryColorA20': 'rgba(3, 7, 18, 0.2)',
+        'secondaryColorA30': 'rgba(3, 7, 18, 0.3)',
         'secondaryColorA40': 'rgba(3, 7, 18, 0.4)',
         'secondaryColorA50': 'rgba(3, 7, 18, 0.5)',
         'secondaryColorA60': 'rgba(3, 7, 18, 0.6)',
